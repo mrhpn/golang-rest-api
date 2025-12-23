f5f0a9ab55394b32a8da36cdf59876c7f88cdab0
mrhpn <mr.htetphyonaing@gmail.com>
2025-12-24 01:25:15 +0630

    Update graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,50 +20,40 @@ import (
 )
 
 func main() {
-	// ----- ✅ load env ----- //
+	// ----- ✅ 1. load env & configs ----- //
 	_ = godotenv.Load()
-
-	// ----- ✅ load configs ----- //
 	cfg := config.Load()
 
-	// ----- ✅ connect to database ----- //
+	// ----- ✅ 2. setup logger ----- //
+	logger := app.SetupLogger(cfg.AppEnv)
+	log.Logger = logger
+
+	// ----- ✅ 3. connect to database ----- //
 	db, err := database.Connect(cfg.DBUrl)
 	if err != nil {
 		log.Fatal().Err(err).Msg("❌ Database connection failed")
 	}
-	// ensure database connection is closed on exit
-	defer func() {
-		sqlDB, _ := db.DB()
-		sqlDB.Close()
-		log.Info().Msg("✅ Database connection closed")
-	}()
 
-	// ----- ✅ setup logger ----- //
-	logger := app.SetupLogger(cfg.AppEnv)
-	log.Logger = logger
-
-	// ----- ✅ register validators ----- //
+	// ----- ✅ 4. register validators ----- //
 	httpx.RegisterValidators()
-
-	// ----- ✅ setup router and register routes ----- //
-	router := gin.New()
-	// recovery panic
-	router.Use(middlewares.Recovery())
-	// register route-request logger
-	router.Use(middlewares.RequestLogger())
-	// register request-id middleware
-	router.Use(middlewares.RequestID(cfg.AppEnv))
-	// register routes
-	routes.Register(router, db)
-
-	// ----- ✅ setup (start/stop) HTTP server ----- //
 	if cfg.AppEnv != "development" {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
+	// ----- ✅ 5. setup router and register routes ----- //
+	router := gin.New()
+	router.Use(middlewares.Recovery())
+	router.Use(middlewares.RequestID(cfg.AppEnv))
+	router.Use(middlewares.RequestLogger())
+	routes.Register(router, db)
+
+	// ----- ✅ 6. setup (start/stop) HTTP server ----- //
 	srv := &http.Server{
-		Addr:    ":" + cfg.Port,
-		Handler: router,
+		Addr:         ":" + cfg.Port,
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
 	// start server in a goroutine
@@ -71,23 +61,39 @@ func main() {
 	defer stop()
 
 	go func() {
-		log.Info().Msg("🚀 HTTP server started on port " + cfg.Port)
+		log.Info().Msgf("🚀 HTTP server started on port %s (env=%s) ", cfg.Port, cfg.AppEnv)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal().Err(err).Msg("❌ HTTP server failed")
+			log.Error().Err(err).Msg("❌ HTTP server failed")
 		}
 	}()
 
 	// wait for shutdown signal
 	<-ctx.Done()
-	log.Info().Msg("⚠️ Shutdown singnal received")
+	log.Info().Msg("1. Shutdown singnal received, starting graceful shutdown...")
 
 	// create context with timeout for graceful shutdown
+	// stops accepting new requests, waits for existing requests to finish for 10s
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	// attempt graceful shutdown
+	log.Info().Msg("2. Shutting down HTTP server...")
 	if err := srv.Shutdown(shutdownCtx); err != nil {
-		log.Error().Err(err).Msg("❌ Graceful shutdown failed")
+		log.Error().Err(err).Msg("❌ HTTP server shutdown failed")
+	} else {
+		log.Info().Msg("3. HTTP server shut down gracefully")
 	}
-	log.Info().Msg("✅ Server exited gracefully")
+
+	// 7 ensure database connection is closed on exit
+	// ----- ✅ 7. close database last ----- //
+	sqlDB, _ := db.DB()
+	if sqlDB != nil {
+		if err := sqlDB.Close(); err != nil {
+			log.Error().Err(err).Msg("❌ Failed to close database connection")
+		} else {
+			log.Info().Msg("4. Database connection closed")
+		}
+	}
+
+	log.Info().Msg("5. Server exited gracefully. Bye!")
 }
